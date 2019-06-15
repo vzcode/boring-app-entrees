a81cdbf6fbf11f510011a8f8d97fa65350c050c4
kevz <vzcodesthings@gmail.com>
2019-06-15 14:47:50 -0700

    Routes rename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // const cors = require('cors');
 const app = express();
-const routes = require('./routes/routes');
+const routes = require('./routes');
 const PORT = process.env.PORT || 8080;
 
 app.use('/', routes);
